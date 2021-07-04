fc3900e7668ba0efa9fff0f6e7c0d2c99866915d
skreq <tvlinkor@gmail.com>
2021-07-05 01:36:30 +0300

    add generate

--- main.py
+++ main.py
@@ -3,13 +3,26 @@ import numpy as np
 import PyQt5
 import random
 #x=symbols('x',real=True)
-x="x"
-a=[[1,2,3],
-[4,5,6],
-[7,8,x]]
-a=np.array(a)
-print(a)
-print("-----------------------------------")
+
+
+def generate(a):
+    a=sorted(a, key=lambda a: random.random())
+    if validate(a):
+        return  a 
+    else:
+        generate(a)
+
+def validate(a):
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            k=1
+            n=0
+    if k>n:
+        return True
+    else:
+        return False
+
+
 def print_pressed_keys(e):
     #print(e, e.event_type, e.name)
     if e.event_type=="down":
@@ -63,5 +76,15 @@ def print_pressed_keys(e):
                 a[k1][k2-1],a[k1][k2]=a[k1][k2],a[k1][k2-1]
             print(a)
             print("-----------------------------------")
+
+x="x"
+a=[[1,2,3],
+[4,5,6],
+[7,8,x]]
+
+a=generate(a)
+a=np.array(a)
+print(a)
+print("-----------------------------------")
 keyboard.hook(print_pressed_keys)
 keyboard.wait()
